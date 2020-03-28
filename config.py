07074de2a0078e5141ca1a891d0a54ea491ec5cf
ChenHongYen <rickchen147258@gmail.com>
2020-03-28 23:34:13 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
+# Below items are fixed
 Input_dir                   = "Input/"
 has_no_behavior_malware_dir = "has_no_behavior_malware/"
 has_behavior_malware_dir    = "has_behavior_malware/"
 not_analysis_dir            = "not_analysis/"
 already_analysis_dir        = "already_analysis/"
 PcapSplitter_path           = "./PcapPlusPlus/Examples/PcapSplitter/Bin/PcapSplitter" # PcapSplitter path
-cuckoo_storage_path         = "/opt/cuckoo/storage/analyses/"
-cuckoo_path                 = "/opt/cuckoo/"
 Csv_dir                     = "csv_report/"
 neccesary_dirs              = ["not_analysis/", "has_no_behavior_malware/", 
 				 			   "has_behavior_malware/", "already_analysis/"]
+				 			   
+# Based on yor setting, below items may be different 	   
+cuckoo_storage_path         = "/opt/cuckoo/storage/analyses/"
+cuckoo_path                 = "/opt/cuckoo/"
 vm_name = "cuckoo1"
